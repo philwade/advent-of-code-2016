c83b4f13487893b74b6268d76c649d0216487854
Phil Wade <phil@whoisstudio.com>
2016-12-05 10:55:08 -0500

    Broken version of day2 part 2

--- day2.elm
+++ day2.elm
@@ -1,13 +1,13 @@
 module Day2 exposing(..)
 import Html exposing(text, div)
-import List exposing(map, foldl, reverse, tail)
+import List exposing(map, foldl, reverse, tail, head)
 
 main = div []
        [ text (allLines directions
                 |> reverse
                 |> tail
                 |> Maybe.withDefault []
-                |> map gridTranslate
+                |> map (getGridValue grid)
                 |> toString)
        ]
 
@@ -16,6 +16,10 @@ type Direction = Up
                | Left
                | Right
                | Nothing
+
+type GridValue = Value String
+               | None
+
 
 parseDirection : String -> Direction
 parseDirection str =
@@ -32,10 +36,10 @@ splitDirections = String.split ""
 directions = map (\x -> splitDirections x |> List.map parseDirection) lines
 
 singleLine : List Direction -> (Int, Int) -> (Int, Int)
-singleLine line startingpoint = foldl (\direction position -> move direction position) startingpoint line
+singleLine line startingpoint = foldl (\direction position -> move2 direction position) startingpoint line
 
 allLines : List (List Direction) -> List (Int, Int)
-allLines directions = foldl (\line stops -> (singleLine line (List.head stops |> Maybe.withDefault (0,0))) :: stops) [(0, 0)] directions
+allLines directions = foldl (\line stops -> (singleLine line (List.head stops |> Maybe.withDefault (0,0))) :: stops) [(0, 2)] directions
 
 gridTranslate : (Int, Int) -> Int
 gridTranslate (x, y) =
@@ -59,6 +63,47 @@ move direction (x, y) =
         Left -> ((subtractFrom x), y)
         Right -> ((addTo x), y)
         Nothing -> (x, y)
+
+move2 : Direction -> (Int, Int) -> (Int, Int)
+move2 direction (x, y) =
+    let
+        newPosition = case direction of
+                Up -> (x, y - 1)
+                Down -> (x, y + 1)
+                Left -> (x - 1, y)
+                Right -> (x + 1, y)
+                Nothing -> (x, y)
+        newValue = getGridValue grid newPosition
+    in
+        case newValue of
+            None -> (x, y)
+            Value _ -> newPosition
+
+
+grid : List (List GridValue)
+grid = [ [ None,      None,      Value "1", None,      None ]
+       , [ None,      Value "2", Value "3", Value "4", None ]
+       , [ Value "5", Value "6", Value "7", Value "8", Value "9" ]
+       , [ None,      Value "A", Value "B", Value "C", None ]
+       , [ None,      None,      Value "D", None,      None ]
+       ]
+
+getGridValue : List (List GridValue) -> (Int, Int) -> GridValue
+getGridValue grid (x,y) = getGridRow y 0 grid |> getGridColumn x 0
+
+getGridRow : Int -> Int -> List (List GridValue) -> List GridValue
+getGridRow index current grid =
+    if index == current then
+        head grid |> Maybe.withDefault [None]
+    else
+        getGridRow index (current + 1) (tail grid |> Maybe.withDefault [[None]])
+
+getGridColumn : Int -> Int -> List GridValue -> GridValue
+getGridColumn index current grid =
+    if index == current then
+        head grid |> Maybe.withDefault None
+    else
+        getGridColumn index (current + 1) (tail grid |> Maybe.withDefault [None])
 
 addTo : Int -> Int
 addTo n =
